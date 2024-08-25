366cc538b2b085a74c06e3c78fb84099e8e77e80
amal velloth <amalv414@gmail.com>
2024-08-26 00:22:25 +0530

    update

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const myText = new SplitType('#my-text')
 
 gsap.to('.char', {
     y: 0,
-    stagger: 0.05,
+    stagger: 0.02,
     delay: 0.2,
-    duration: .1
+    duration: .1,
+    ease: "power1.out"
 })
+
