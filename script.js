50289eb31dbacccaac9723144b4825c50dd1cfb0
amal velloth <amalv414@gmail.com>
2024-09-10 17:43:53 +0530

    section & three box contents

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-const myText = new SplitType('#my-text')
+const myText = new SplitType("#my-text");
 
-gsap.to('.char', {
-    y: 0,
-    stagger: 0.02,
-    delay: 0.2,
-    duration: .1,
-    ease: "power1.out"
-})
+gsap.to(".char", {
+  y: 0,
+  stagger: 0.02,
+  delay: 0.01,
+  duration: 0.1,
+  ease: "power1.out",
+});
 
+
+window.addEventListener("load", function () {
+  const tl = gsap.timeline({
+    onComplete: function () {
+      document.querySelector(".content").style.opacity = 1;
+    },
+  });
+
+  tl.to(".overlay-box1", {
+    duration: 1.5,
+    y: "100%",
+    ease: "power4.inOut",
+    delay: 2.1
+  });
+  tl.to(".overlay-box2", {
+    duration: 1.5,
+    y: "100%",
+    ease: "power4.inOut",
+    delay: -1.2
+  });
+  tl.to(".overlay-box3", {
+    duration: 1.5,
+    y: "100%",
+    ease: "power4.inOut",
+    delay: -1.2
+  });
+});
